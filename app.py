cee8d8a04cc02157840899edc7a1fafbaef492aa
Babin Bohora <bohorababin1@gmail.com>
2022-07-04 21:56:01 +0900

    layout created and return from index refactored with new template

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request
 
+# Configure application
 app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def index():
-  return "HELLLO WORLD"
-  # return render_template("home.html")
+  # return "HELLLO WORLD"
+  return render_template("index.html")
