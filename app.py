5ca4c2ca1e7c9e121f7b8ff4319f7342a8374d86
Babin Bohora <bohorababin1@gmail.com>
2022-07-05 13:57:01 +0900

    more features added

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from pkg_resources import EGG_DIST
 from flask import Flask, render_template, request, redirect, url_for,after_this_request, send_file
 from PIL import Image, ImageFilter
 from werkzeug.utils import secure_filename
@@ -26,7 +27,7 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/image", methods = ["POST", "GET"])
-def blur():
+def image_edit():
 
   # if no photo key on form
   if 'photo' not in request.files:
@@ -39,17 +40,34 @@ def blur():
     return redirect("/")
 
   if photo and allowed_file(photo.filename):
+    edit_type = request.form.get("mode")
+    print("mode", edit_type)
     filename = secure_filename(photo.filename)
     img = Image.open(photo)
-    img = img.convert('L')
+
+    # editing images based on different mode.
+    if edit_type == "grayScale":
+      img = img.convert('L')
+
+    elif edit_type == "emboss":
+      img = img.filter(filter=ImageFilter.EMBOSS)
+
+    elif edit_type == "blur":
+      img = img.filter(filter=ImageFilter.BoxBlur(3))
+
+    else:
+      img = img.filter(ImageFilter.FIND_EDGES)
+
+
     img.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     return redirect(url_for('output', name=filename))
 
 @app.route("/output/<name>")
 def output(name):
   filepath = os.path.join(app.config['UPLOAD_FOLDER'], name)
+
   if os.path.isfile(filepath):
-    return render_template("output.html", filepath=filepath, name=name)
+    return render_template("output.html", name=name)
 
   return redirect("/")
 
@@ -57,8 +75,10 @@ def output(name):
 @app.route('/uploads/<name>')
 def download_file(name):
     filepath = os.path.join(UPLOAD_FOLDER, name)
+
     if not os.path.isfile(filepath):
       redirect("/")
+
     @after_this_request
     def remove_file(response):
       os.remove(filepath)
@@ -69,6 +89,7 @@ def download_file(name):
 @app.route("/delete/<name>")
 def delete_file(name):
   filepath = os.path.join(UPLOAD_FOLDER, name)
+
   if os.path.isfile (filepath):
     os.remove(filepath)
   return redirect("/")
